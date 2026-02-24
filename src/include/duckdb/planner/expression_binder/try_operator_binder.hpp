f5b6a6bd4ce1604d02fb5565ef693d7a0849571a
ArNine <851332120@qq.com>
2026-02-24 18:03:53 +0800

    test

--- src/include/duckdb/planner/expression_binder/try_operator_binder.hpp
+++ src/include/duckdb/planner/expression_binder/try_operator_binder.hpp
@@ -19,6 +19,7 @@ class TryOperatorBinder : public ExpressionBinder {
 public:
 	TryOperatorBinder(Binder &binder, ClientContext &context);
 
+	//! This binder is used for the TRY expression
 	bool TryResolveAliasReference(ColumnRefExpression &colref, idx_t depth, bool root_expression, BindResult &result,
 	                              unique_ptr<ParsedExpression> &expr_ptr) override {
 		if (!stored_binder) {
